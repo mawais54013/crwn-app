c7ee123d567bcfa8ff4f1837c7128291ad113e22
MAwais <mawais54013@outlook.com>
2019-09-07 16:25:06 -0700

    added signin

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import HomePage from "./pages/homepage/homepage.component";
+import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-out/sign-in-and-sign-out';
 import './App.css';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/sign-in-and-sign-out/sign-in-and-sign-out.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import './sign-in-and-sign-out.styles.scss';
+
+const SignInAndSignUpPage = () => (
+    <div className="sign-in-and-sign-out">
+        SIGN IN
+    </div>
+);
+export default SignInAndSignUpPage;
