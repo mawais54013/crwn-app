cbb6596d935141baec698c6ff6f3c7482904ec76
MAwais <mawais54013@outlook.com>
2019-09-16 22:06:40 -0700

    finished sign in and signout features

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -19,9 +19,43 @@ class SignUp extends React.Component {
         }
     }
 
+    handleSubmit = async event => {
+        event.preventDefault();
+
+        const {displayName, email, password, confirmPassword } = this.state;
+
+        if(password !== confirmPassword)
+        {
+            alert("passwords don't match");
+            return;
+        }
+
+        try {
+            const { user } = await auth.createUserWithEmailAndPassword(email, password);
+
+            await createUserProfileDocument(user, { displayName });
+
+            this.setState({
+                displayName: '',
+                email: '',
+                password: '',
+                confirmPassword: ''
+            })
+
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    handleChange = event => {
+        const { name, value } = event.target;
+
+        this.setState({ [name]: value });
+    }
+
     render() 
     {
-        const {displayName, email, password, confirmPassword } = this.state;
+        const { displayName, email, password, confirmPassword } = this.state;
         return (
             <div className="sign-up">
                 <h2 className="title">I do not have a account</h2>
@@ -35,8 +69,11 @@ class SignUp extends React.Component {
 
                     <FormInput type="password" name="confirmPassword" value={confirmPassword} onChange={this.handleChange} label="Confirm Password" required/>
 
+                    <CustomButton type='submit'>SIGN UP</CustomButton>
                 </form>
             </div>
         )
     }
 }
+
+export default SignUp;
