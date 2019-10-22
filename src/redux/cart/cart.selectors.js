73065d67da34c076cbe71c770ca308d299cde120
MAwais <mawais54013@outlook.com>
2019-10-21 22:59:41 -0700

    update

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -1,22 +1,33 @@
 import { createSelector } from 'reselect';
 
-const selectedCart = state => state.cart;
+const selectCart = state => state.cart;
 
-export const selectedCartItems = createSelector(
-    [selectCart],
-    (cart) => cart.cartItems
+export const selectCartItems = createSelector(
+  [selectCart],
+  cart => cart.cartItems
 );
 
 export const selectCartHidden = createSelector(
-    [selectCart],
-    cart => cart.hidden
-)
+  [selectCart],
+  cart => cart.hidden
+);
 
 export const selectCartItemsCount = createSelector(
-    [selectedCartItems],
+  [selectCartItems],
+  cartItems =>
     cartItems.reduce(
-        (accumalatedQuantity, cartItem) => 
-        accumalatedQuantity + cartItem.quantity, 
-        0 
+      (accumalatedQuantity, cartItem) =>
+        accumalatedQuantity + cartItem.quantity,
+      0
+    )
+);
+
+export const selectCartTotal = createSelector(
+  [selectCartItems],
+  cartItems =>
+    cartItems.reduce(
+      (accumalatedQuantity, cartItem) =>
+        accumalatedQuantity + cartItem.quantity * cartItem.price,
+      0
     )
 );
